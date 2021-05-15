e73e27841547e644381875871deb2776d7b05964
darknessk2 <59013970+darknessk2@users.noreply.github.com>
2021-05-15 21:15:44 +0700

    Add files via upload

--- bug.route.js
+++ bug.route.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = (app) => {
 
   app.get('/bugs', (req, res) => {
-    connection.query("SELECT * FROM bugtracker.bug_report;", (err, result) => {
+    connection.query("SELECT BIN_TO_UUID(bug_id),title,bug_description,bug_status,BIN_TO_UUID(userID) FROM bugtracker.bug_report;", (err, result) => {
       if (err) throw err;
       res.send(result);
     });
@@ -13,7 +13,16 @@ module.exports = (app) => {
 
   app.get('/bugs?staffID=:id', (req, res) => {
     let id = req.params.id;
-    let sql = `USE bugtracker; SELECT * FROM bug_report b, work_on w, people p WHERE b.bug_id = w.bug_id AND p.id = ${id}`;
+    let sql = `SELECT *,BIN_TO_UUID(bug_report.bug_id),BIN_TO_UUID(work_on.staffID) FROM bug_report INNER JOIN work_on ON  BIN_TO_UUID(work_on.staffID) = ${id}`
+    connection.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  })
+
+  app.get('/bugs?userID=:id', (req, res) => {
+    let id = req.params.id;
+    let sql = `USE bugtracker; SELECT *,BIN_TO_UUID(bug_id) , BIN_TO_UUID(userID) FROM bug_report b WHERE b.bug_status = 0 AND b.userID = UUID_TO_BIN(${id})`
     connection.query(sql, (err, result) => {
       if (err) throw err;
       res.send(result);
@@ -30,14 +39,16 @@ module.exports = (app) => {
     const time = data.updates.time;
     const content = data.updates.content;
     const authorID = data.updates.authorID;
-
-    sql1 = `INSERT INTO bug_report(bug_id,title,bug_description,status) VALUES (UUID_TO_BIN(${UUID}), "${title}", "${description}", 0)`;
+    sql1 = `INSERT INTO bug_report(bug_id,title,bug_description,bug_status,userID) VALUES (UUID_TO_BIN(${UUID}), "${title}", "${description}", 0, ${userID})`;
     sql2 = `INSERT INTO work_on(bug_id,staffID) VALUES(UUID_TO_BIN(${UUID}),${staffID})`;
-    sql3 = `INSERT INTO bug_update(bug_id,content,update_time) VALUES (UUID_TO_BIN(${UUID}),"${content}", ${time})`;
-
+    sql3 = `INSERT INTO bug_update(bug_id,content,update_time,authorID) VALUES (UUID_TO_BIN(${UUID}),"${content}", ${time}, ${authorID})`;
     connection.query(sql1);
     connection.query(sql2);
     connection.query(sql3);
+  })
 
+  app.delete('/bugs/:bugID', (req,res) => {
+    let bugID = req.params.bugID;
+    connection.query(`SET SQL_SAFE_UPDATES = 0; USE bugtracker; UPDATE bug_report SET bug_status = 1 WHERE BIN_TO_UUID(bug_id) = "${bugID}"`)
   })
 }
